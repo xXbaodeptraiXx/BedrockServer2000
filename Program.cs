bd1c60554a93137d93d383b56e318c09cb4052ea
Lâm Gia Bảo <lamgiabao1456@gmail.com>
2021-06-13 22:13:36 +0700

    removed debug code

--- Program.cs
+++ Program.cs
@@ -17,7 +17,7 @@ namespace BedrockServer2000
 		static void Main()
 		{
 			// debug line
-			Directory.SetCurrentDirectory("/home/bao/bedrock_server");
+			// Directory.SetCurrentDirectory("/home/bao/bedrock_server");
 
 			// Process
 			AppDomain.CurrentDomain.UnhandledException += new UnhandledExceptionEventHandler(Events.OnExit);
