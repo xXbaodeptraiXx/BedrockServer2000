a571a6390445d52ecd896a6515607b7f7cb06ffb
Arman Mokammel <armanmokammel@gmail.com>
2021-08-15 22:09:59 +0600

    Added new methods
    Added new methods for `ConsoleEx.WriteLine()` & `Logger.Log()`.

---
+++ source/ConsoleEx.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace BedrockServer2000
+{
+    public static class ConsoleEx
+    {
+        public static void WriteLine(string message)
+        {
+            Console.WriteLine(message);
+            Logger.Log(message);
+        }
+    }
+}
---
+++ source/Logger.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace BedrockServer2000
+{
+    static class Logger
+    {
+        public static StreamWriter writer;
+
+        public static void Log(string logMessage)
+        {
+            writer.WriteLine($"{Timing.LogDateTime()}  :{logMessage}");
+            writer.WriteLine("-------------------------------");  
+        }
+    }
+}
