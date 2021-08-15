32e0f30f97b7eb20902f43681ba0c2fff6362ca7
Arman Mokammel <armanmokammel@gmail.com>
2021-08-15 22:34:53 +0600

    Update Events.cs
    Updated to use the new `ConsoleEx.WriteLine()` & `Logger.Log()` methods.

--- source/Events.cs
+++ source/Events.cs
@@ -25,7 +25,7 @@ namespace BedrockServer2000
 
 			Program.exitTImeoutTImer.Change(Timeout.Infinite, Timeout.Infinite);
 
-			Console.WriteLine($"{Timing.LogDateTime()} Server stopped.");
+			ConsoleEx.WriteLine($"{Timing.LogDateTime()} Server stopped.");
 
 			if (Program.LoadRequest)
 			{
@@ -33,7 +33,7 @@ namespace BedrockServer2000
 			}
 			else if (Program.ExitRequest)
 			{
-				Console.WriteLine($"{Timing.LogDateTime()} Server wrapper stopped.");
+				ConsoleEx.WriteLine($"{Timing.LogDateTime()} Server wrapper stopped.");
 				Environment.Exit(0);
 			}
 		}
@@ -41,10 +41,12 @@ namespace BedrockServer2000
 		public static void BedrockServerProcess_OutputDataReceived(object sender, DataReceivedEventArgs e)
 		{
 			if (e.Data == null) return;
+			
+			Logger.Log(e.Data);
 
 			if (e.Data == "Data saved. Files are now ready to be copied." || e.Data == "Saving...")
 			{
-				Console.WriteLine($"{Timing.LogDateTime()} {e.Data}");
+				ConsoleEx.WriteLine($"{Timing.LogDateTime()} {e.Data}");
 				return;
 			}
 
@@ -55,7 +57,7 @@ namespace BedrockServer2000
 				&& e.Data.Contains(":")
 				&& e.Data.Contains(", "))
 				{
-					Console.WriteLine($"{Timing.LogDateTime()} Raw file list received.");
+					ConsoleEx.WriteLine($"{Timing.LogDateTime()} Raw file list received.");
 					Backup.PerformOnlineBackup(e.Data);
 					return;
 				}
@@ -68,7 +70,7 @@ namespace BedrockServer2000
 			string outputData = e.Data;
 			// remove "NO LOG FILE - " from the server's output data
 			if (outputData.StartsWith("NO LOG FILE! - ")) outputData = outputData.Remove(0, 15);
-			Console.WriteLine($"{Timing.LogDateTime()} {outputData}");
+			ConsoleEx.WriteLine($"{Timing.LogDateTime()} {outputData}");
 
 			if (e.Data.Contains("[INFO] Player connected: "))
 			{
@@ -93,17 +95,17 @@ namespace BedrockServer2000
 			// check if the configs are correct, cancel the backup if found any error
 			if (!Directory.Exists((string)Program.ServerConfigs["worldPath"]))
 			{
-				Console.WriteLine($"World path incorrect, can't perform backup.");
+				ConsoleEx.WriteLine($"World path incorrect, can't perform backup.");
 				return;
 			}
 			if (!Directory.Exists((string)Program.ServerConfigs["backupPath"]))
 			{
-				Console.WriteLine($"Backup path incorrect, can't perform backup.");
+				ConsoleEx.WriteLine($"Backup path incorrect, can't perform backup.");
 				return;
 			}
 			if ((int)Program.ServerConfigs["backupLimit"] <= 0)
 			{
-				Console.WriteLine($"Backup limit can't be smaller than 1, can't perform backup.");
+				ConsoleEx.WriteLine($"Backup limit can't be smaller than 1, can't perform backup.");
 				return;
 			}
 
@@ -122,7 +124,7 @@ namespace BedrockServer2000
 				{
 					if (player.Name == ((string[])Program.ServerConfigs["banList"])[i])
 					{
-						Console.WriteLine($"{Timing.LogDateTime()} Player name \"{player.Name}\" found in ban list.");
+						ConsoleEx.WriteLine($"{Timing.LogDateTime()} Player name \"{player.Name}\" found in ban list.");
 						Program.serverInput.WriteLine($"kick {player.Name}");
 					}
 				}
@@ -133,9 +135,9 @@ namespace BedrockServer2000
 		{
 			if (!Program.ExitCompleted)
 			{
-				Console.WriteLine($"{Timing.LogDateTime()} Exit timed out.");
+				ConsoleEx.WriteLine($"{Timing.LogDateTime()} Exit timed out.");
 				Program.serverProcess.Kill();
-				Console.WriteLine($"{Timing.LogDateTime()} Force killed server process.");
+				ConsoleEx.WriteLine($"{Timing.LogDateTime()} Force killed server process.");
 				Program.ExitCompleted = true;
 
 				Program.exitTImeoutTImer.Change(Timeout.Infinite, Timeout.Infinite);
@@ -155,7 +157,7 @@ namespace BedrockServer2000
 			{
 				if (playerName == player.Name)
 				{
-					Console.WriteLine($"{Timing.LogDateTime()} Player name \"{playerName}\" found in ban list.");
+					ConsoleEx.WriteLine($"{Timing.LogDateTime()} Player name \"{playerName}\" found in ban list.");
 					AutoKick(playerName, 5000);
 					break;
 				}
