c0a1d1911a0a2c5618c76a1cb5f26d69162075fe
Arman Mokammel <armanmokammel@gmail.com>
2021-08-17 15:39:26 +0600

    Update ConsoleEx.cs

--- source/ConsoleEx.cs
+++ source/ConsoleEx.cs
@@ -4,10 +4,18 @@ namespace BedrockServer2000
 {
     public static class ConsoleEx
     {
+        public static StreamWriter writer;
+        
         public static void WriteLine(string message)
         {
             Console.WriteLine(message);
             Logger.Log(message);
+        }
+
+        public static void Log(string logMessage)
+        {
+            writer.WriteLine($"{Timing.LogDateTime()}  :{logMessage}");
+            writer.WriteLine("-------------------------------");  
         }
     }
 }
